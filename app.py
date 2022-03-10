d39688d1f330b2e611c0b67229a3699d8a4d5d72
akotronis <akotronis@gmail.com>
2022-03-10 16:10:19 +0200

    Made run.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
-from db import db
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from resources.user import UserRegister
 from security import authenticate, identity
+
 
 app = Flask(__name__)
 app.secret_key = 'ak'
@@ -16,11 +16,6 @@ api = Api(app)
 jwt = JWT(app, authenticate, identity) # Creeates a new endpoint /auth
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
@@ -28,5 +23,4 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
-    db.init_app(app)
     app.run(port=5000, debug=True)
